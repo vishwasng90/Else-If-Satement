19d32deeeb6fe282912ba6448ea88d5cfe3ac49e
vishwasng90 <164770369+vishwasng90@users.noreply.github.com>
2024-06-11 12:11:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 24;
+            if(time<10)
+            {
+                Console.WriteLine("Good Morning");
+            }
+            else if(time<18)
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+            else
+            {
+                Console.WriteLine("Good evening");
+                Console.ReadLine();
+            }
+        }
+    }
+}
